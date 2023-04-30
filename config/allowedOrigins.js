ca6bd0394d26f85a74b6774ca674776353ddaf88
mohammed mouhine <mouhinemohmade@gmail.com>
2023-04-30 11:32:09 +0100

    cors fix

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,10 +1,6 @@
 const allowedOrigins = [
   "http://localhost:3000",
-  "https://socialmediaapp-git-main-mouhine.vercel.app/",
-  "https://socialmediaapp-git-main-mouhine.vercel.app",
-  "https://socialmediaapp-goll.vercel.app",
-  "https://main--shiny-sfogliatella-3a3566.netlify.app",
-  "https://main--shiny-sfogliatella-3a3566.netlify.app/",
+ "https://full-stack-social-media-client.vercel.app/"
 ];
 
 module.exports = allowedOrigins;
